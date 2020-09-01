abe1c410037552474471fbbddc93dc1d10fc4e98
David-Carranza <dcarranza.business@gmail.com>
2020-08-31 21:48:47 -0700

    upgraded centering logic and added Jasp's pillar

--- Assets/Scripts/Level/Board.cs
+++ Assets/Scripts/Level/Board.cs
@@ -7,8 +7,10 @@ public class Board : MonoBehaviour
     public static Board Instance { get; private set; }
 
     [SerializeField]
-    private GameObject Terrain;
-    
+    private GameObject TerrainObject;
+    [SerializeField]
+    private GameObject BoardObject;
+
     void Start()
     {
         Instance = this;
@@ -21,12 +23,16 @@ public class Board : MonoBehaviour
 
     public void SetGrid(int blockWidth, int blockHeight)
     {
-        Material material = GetComponent<Renderer>().material;
+        Material material = BoardObject.GetComponent<Renderer>().material;
         material.mainTextureScale = new Vector2(blockWidth/2.0f, blockHeight/2.0f);
-        transform.localScale = new Vector3(blockWidth, blockHeight, 1.0f);
-        transform.localPosition = new Vector3
-            ((blockWidth - 1.0f)/2, -0.5f, (blockHeight - 1.0f)/ 2);
+        BoardObject.transform.localScale = new Vector3(blockWidth, blockHeight, 1.0f);
+        BoardObject.transform.localPosition = new Vector3
+            ((blockWidth - 1.0f)/2, -0.5f, (blockHeight - 1.0f)/ 2.0f);
+        
+        float factor = Mathf.Clamp01((blockWidth - 8)/8.0f);
 
-        Terrain.transform.localPosition = new Vector3(-2.0f, 0.0f, blockHeight + 1.0f);
+        TerrainObject.transform.localPosition = new Vector3(-8.0f + factor * 3.0f, -14.0f, blockHeight + 2.0f);
+        
+        transform.localPosition = new Vector3 (blockWidth/-2.0f, -0.5f, blockHeight/-2.0f);
     }
 }
